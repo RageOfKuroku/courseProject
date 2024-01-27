fc04cdff5f65cdb87c5dbfea96bd507890e0ad4e
Egor.Efimenko <20_Morokey_04@mail.ru>
2024-01-27 16:27:56 +0300

    redirection attempt

--- MainFile/src/main/java/com/example/mainfile/web/HotelController.java
+++ MainFile/src/main/java/com/example/mainfile/web/HotelController.java
@@ -8,47 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-@RequiredArgsConstructor
 @Controller
-@RequestMapping("/admin")
+@RequiredArgsConstructor
+@RequestMapping("/hotels")
 public class HotelController {
 
     private final HotelService service;
-
-    @GetMapping("/addHotel")
-    public String showAddHotelForm(Model model) {
+    @GetMapping
+    public String showHotelForm(Model model) {
         model.addAttribute("hotel", new HotelDto());
         model.addAttribute("hotels", service.getAllHotels());
-        return "adminPage";
-    }
-    @GetMapping
-    public List<HotelDto> getAllHotels() {
-        return service.getAllHotels();
+        return "hotelsPage";
     }
 
-    @GetMapping("/{id}")
-    public HotelDto getHotelById(@PathVariable Integer id) {
-        return service.getHotelById(id);
+    @PostMapping("/hotels/{id}")
+    public String showHotelDetails(@PathVariable Integer id, Model model) {
+        HotelDto hotel = service.getHotelById(id);
+        model.addAttribute("hotel", hotel);
+        return "hotelPage";
     }
-
-    @PostMapping("/addHotel")
-    public String addHotelRedirection(@ModelAttribute("hotel") HotelDto hotelDto, RedirectAttributes redirectAttributes) {
-        service.createHotel(hotelDto);
-        redirectAttributes.addFlashAttribute("message", "Отель успешно добавлен!");
-        return "redirect:/admin/addHotel";
-    }
-
-    @PutMapping("/{id}")
-    public HotelDto updateHotel(@PathVariable Integer id, @RequestBody HotelDto hotelDto) {
-        return service.updateHotel(id, hotelDto);
-    }
-
-    @PostMapping("/delete/{id}")
-    public String deleteHotel(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
-        service.deleteHotel(id);
-        redirectAttributes.addFlashAttribute("message", "Отель успешно удалён!");
-        return "redirect:/admin/addHotel";
-    }
-
 }
