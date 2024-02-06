8e928b8c567d26eab22e53280109112c0b88572c
Egor.Efimenko <20_Morokey_04@mail.ru>
2024-02-06 21:11:19 +0300

    adding (non)important files

---
+++ MainFile/src/main/java/com/example/mainfile/MainFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainFileApplication.class, args);
+    }
+
+}
---
+++ MainFile/src/test/java/com/example/mainfile/MainFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
